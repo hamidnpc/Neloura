e425b9cd734bce65f3e4696678111d5c203cc837
hamidnpc <hamidhassani_ir@yahoo.com>
2025-02-13 13:01:46 +0100

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -12,11 +12,11 @@ import json
 
 SCOPES = ["https://www.googleapis.com/auth/drive.file"]
 
-CREDENTIALS_DICT = {
+CREDENTIALS_DICT={
   "type": "service_account",
   "project_id": "webapp-450712",
-  "private_key_id": "5240068958080a00dc7ec892acf3082afdcf1f70",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCR8C4Zys/nFmlG\nMW5egjE0/wUi6OTlwXO5wzhhp6y/bxxrW5n5Ez7nU49C7TF09GjTvhMsCwKCv3RV\nd+AVRyaeZolsS10dt0ICBppRtiucdpxDMOw8hrllfdcuiVNoGMHrEQjfQDbNfOMN\npOuWJg3hCR66yxNCmTBdU1Yyx5lt5/5HQkBSE5B4x/b24vObOPUiaD2XaIDXLUv9\nCwge8A6xgrH+aZXUxJ7oh0R4Bi7BFfBgtN7E6+F469P6Asxnm3Yxhyt53RRGH1A5\noVHARiR+SbGyCQokdw4YFiYrGLoHBKs1bumxUT3vwq/StM3pKV8GEttAdsTqCgkr\nJmCKEJWzAgMBAAECggEAD24OtGu/mztrimA2lFJ9Q7uvk4HT6xY5l0tJ6PVSq/OM\n/y2HtDsx3EEhoAi2a8WzAamSmwZXpR2l/gAMgWNs9LQlqn1gt9VvlV7qWQ0C7mo8\nr6lQCOmRAjyRuu9fvLKwFMZPzambQb2xv026b5J6xtob1Xxxfwn1qvW2jZbnT3ne\nMVNl39e+V98LT6k8IlC2GQV08wcUKIYb1bgxIUAJqQmFwYv1/wJnsabNmOtRA469\nx8oWKQFvQ/MxSiHkJlIdoSNxqGEJXlaSzZETgEdnoAad7svCU6Y2K3BJeMBsLfa+\nbecYf/Z2ZTzLBiHwB5/Kbhqo5xXJA4nJoCfM+M8YVQKBgQDCBUxwiIZHsvTbmkkd\neLz+ZD/fsI920kbohx7YgW8fOvbvQVw/iJnxZxZFOT+7rWQVMw4+yfAlTqRuGPuW\noff33fjgHbOH0ei6TGX847gLV7e8G3WqeBpGl7APBrOhtTjBR8KOsXXxzIj7tX2m\n7E8czrDQlxZMKtKDbYhuqagCfQKBgQDAjsWyA0lu/Qf8wEo28q01fe61aD9joEnA\nfTGUuWwD7yQRsCAkAotpVVXCWecBxs1cm9t+196X+bD10wTr3H961PKjndNKbmPs\nsybvaLRNnyaOBX8tuHLBkPhoyjDVEJjb/p/CvuuoN2DUzqo/0zDq/6Kon9tDBdB4\nRresKbe/7wKBgHF/rAHAu9zBoV+917u338dpWbw/iw1VYUmxkafRv/GjCsVXlOFN\nxdBkH7ELFm9xtTR5k4HDnAoXATWuw66QvYBwh6CDf3yanOwuuRLBG+72t3MBFUpN\nzAEzxzhjET4txy+6DORwh7CVmCS3PPQjFPyjJOhBGjggbNmZf9BMyw35AoGBALvq\nWSpJJN/olWPm8EZBfCGo3U0yCDKcmXz9cGZ4sd4BAg00+ZjK1uybt86sV9GXBpDr\nFUDrQDLGHRk5ryLSk6H2sThVbH7FzkSP7V5UqyjZORH+cUfYKj2W0aXKM++qe9Kt\n4ZF3KpmGObxUVzdkTef4Q/AV2S/GO9U1BkhAii8HAoGACSW1EAXNtLS+sWoCAhqp\nbg9qJcN3B10GSZY0NwKEYjXnCSY4ul1DLhxzfTKwsTl7IyD2pjzaG2yWCb4W2Fav\n6gOoC8AJjMmB04jN2+odbGLsSGqBv+rNuKSrIhvGtNrF9+Jp8fw+YUCQaQiWTxvS\nRqeUqfbaUufXf3UB5qJsgoM=\n-----END PRIVATE KEY-----\n",
+  "private_key_id": "b84650d945bc1070310c9dfc1dbdfd280c10b856",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDEI6XOVzk+ZGae\ntefRHvFRjOhyO86F1bH3wafgdrED4ZXa7ULts9OsPW90zLs9AyVx5YnXHddlru8c\n7wlnFNyFztcxHLeqWleO5kEH/e7CjLZe8DZaCoGdAwtQ8c5EpjFlDKAVg777fwZa\nlVXIxRYk+7w/jwCuq+kUfLh2zk5hd0jlMxE/m/OMKKFYW+F9qM2CJ0p+tMHuWg3l\nPS63X+rVDeDSRdIIssNjemGieYW7oopCClqp/Rlsp01Y2NT7TkjWMGMDH0CiBTWH\nyyLvE/sWEsgoUmfN8QQnObCCQnDD1m2TjSFATD4WBfDG5g/E1KpAwyK+sPn+FgEC\nrk+3wwDtAgMBAAECggEADWWC6mpGOGb6pF330K58nI2ThdPkafs7zdiOCxSISCPV\nDLA2aU5mR4YlsfRW2Ja5rMMZDFaXMvSFE/SWr5H68u+6/nl/6PSFNPcwm2dicqrS\nPP9KsxEzI6KE2Bk/9avlRUncuEN4nKxjJnY7NvyRI+MGy0+M5CFNSO8w2TqDm3//\noAx7GWG5eMo0eVKmdx6BS/2bzA3qbLbMMxw152i7S5pBKo10lNfw0e9olRdYYI6q\n7+u8T+bCwVN8DQaCwnKxa6sSR9mmi31uz/8u+MydUJ1U34c29MCTyd+t5gVtAQns\nVXmwxLzbkEAgi/Piaig0UwR6W0pUPqcpAr5GnktDYQKBgQD+oL8Z2kyYFrJqkW5O\nJtyBl3M/aZDx/dYwaQYnpaWMsLCh+MTxtY2UXDPT1iwrmW3eedaa9uYR6zY7SpJc\n9W7JznEMT3D85j6xjoslWpyUDwKfxh/i70bjqMhmqTQZflc2pi0i+yX6o5Aglqt0\n35pZLKEZ79dwZ8Fs+tf1oJodIQKBgQDFMjemeo1YmTXNWCgi/Jwlkhd3m2IJd0lg\n1+HtT/GaL//sZ+pqAjgXFj8akrNweOAvs6HXik8NkBjurscLv7s3WYCEgHhpRWum\nl6PRmexWL6VXKMmA4fN8aqC2bWPllA10hwfggaZpIS5obPDZ1NmjCNld0iulo+sq\nlzoHatJ+TQKBgDV6BSik1FlMywPer0/ykXUWHbo/rZVlPkrEg+pnu7EkmrQpzxME\nkkuKYcgnvvcZLh3/cLbzC7bPs4AYVUz4rt/rX4q6VHyIzrnzuf2KXkAqNt7feWrr\n3fOKSyxkdPC1vBNaKzihjoRpPafafP/6xivcV6le2dxdrh35QMnxhOyBAoGAcH9x\nmBwaPw1I0FkWrL8IJbbuhGG00EmpJwKB9WzCHIB9tFSkOs4BScp7PaGrlOKgoxYL\nondZGhe24ZP8IgvDtpPs6aqRBx1/iI7B+nWhipmr7clrC4fb7IK4JNrLPtHzyyKt\nr7rHQPqZCyH7VYpEjuohd5DLLbgSFT4o8MpUQ5ECgYAE6UZeRlZuBGQhF1NcQHwY\n7UbSlVnihKMG7Oann5Mu3sDC6SUf6dBRZoVz4kGzOLbxZOj98aIEiobqHfVgmdKc\nwaXoeyGfpcBW90Z2GX2hDcp0kFxLebPidI47yrz1U2wut6JotQ8mdxoKQDEhRZqW\n6uGYvhZz3mh/QTq1itJXqQ==\n-----END PRIVATE KEY-----\n",
   "client_email": "aseman@webapp-450712.iam.gserviceaccount.com",
   "client_id": "115492359670944248112",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -25,6 +25,7 @@ CREDENTIALS_DICT = {
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/aseman%40webapp-450712.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 }
+
 
 def authenticate_drive():
     creds = Credentials.from_service_account_info(CREDENTIALS_DICT, scopes=SCOPES)
