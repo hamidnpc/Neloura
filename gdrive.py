37b40157557135ec7358f377a110e815c346d72c
hamidnpc <hamidhassani_ir@yahoo.com>
2025-02-17 16:43:31 +0100

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -6,15 +6,33 @@ from google.oauth2.service_account import Credentials
 import io
 import logging
 
-SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]  # Recommended for listing files
 
 # Load credentials from Railway environment variable
 CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS")
 CREDENTIALS_DICT = json.loads(CREDENTIALS_JSON)  # Convert JSON string to dictionary
 
+
+SCOPES = ["https://www.googleapis.com/auth/drive"]
+
 def authenticate_drive():
-    creds = Credentials.from_service_account_info(CREDENTIALS_DICT, scopes=SCOPES)
-    return build("drive", "v3", credentials=creds)
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            creds_data = json.loads(os.getenv('GOOGLE_OAUTH_CREDENTIALS'))
+            flow = InstalledAppFlow.from_client_config(creds_data, SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('drive', 'v3', credentials=creds)
+
 
 def list_drive_files():
     service = authenticate_drive()
